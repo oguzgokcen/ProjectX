f96bbfc0556abf796d537e23b3c007f7daac75dc
AdemSoydan <ademm.soydan@gmail.com>
2022-12-01 16:34:32 +0300

    add game and favorite buttons and make fragment transactions

--- app/src/main/java/com/example/projectx/MainActivity.kt
+++ app/src/main/java/com/example/projectx/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.projectx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Debug
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var gameButton : Button
+    lateinit var favouritesButton : Button
+    lateinit var gameFragment : GameFragment
+    lateinit var favouritesFragment : Favourites
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createFragmentsAndAssignGameFirst()
+        bindButtons()
+        setButtonsListeners()
+
+    }
+
+    private fun createFragmentsAndAssignGameFirst(){
+        gameFragment = GameFragment()
+        favouritesFragment = Favourites()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, gameFragment)
+            commit()
+        }
+    }
+
+    private fun bindButtons(){
+        gameButton = findViewById(R.id.gameButton)
+        favouritesButton = findViewById(R.id.favouritesButton)
+    }
+
+    fun setButtonsListeners(){
+
+        gameButton.setOnClickListener {
+            gameButton.setTextColor(getApplication().getResources().getColor(R.color.activebuttonbluecolor))
+            favouritesButton.setTextColor(getApplication().getResources().getColor(R.color.notactivebuttonbluecolor))
+            gameButton.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.gamebuttondrawable, 0, 0);
+            favouritesButton.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.favnotselecteddrawable, 0, 0);
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, gameFragment)
+                commit()
+            }
+        }
+
+        favouritesButton.setOnClickListener {
+            favouritesButton.setTextColor(getApplication().getResources().getColor(R.color.activebuttonbluecolor))
+            gameButton.setTextColor(getApplication().getResources().getColor(R.color.notactivebuttonbluecolor))
+            gameButton.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.gamenotselectedbuttondrawable, 0, 0);
+            favouritesButton.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.favdrawable, 0, 0);
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, favouritesFragment)
+                commit()
+            }
+        }
+
     }
 }
