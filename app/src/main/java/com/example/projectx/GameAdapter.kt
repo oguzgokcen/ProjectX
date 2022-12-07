4621a52fd29ff1acf8ece9a980e8a402550a4069
Emir <emiryegnidemir@hotmail.com>
2022-12-07 18:08:21 +0300

    add search view

--- app/src/main/java/com/example/projectx/GameAdapter.kt
+++ app/src/main/java/com/example/projectx/GameAdapter.kt
@@ -1,56 +1,43 @@
 package com.example.projectx
-
-import android.content.Intent
 import android.view.LayoutInflater
-import android.view.View
-import android.view.View.OnClickListener
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.projectx.databinding.ItemGameBinding
 
-import com.bumptech.glide.Glide
-
-
-class GameAdapter(
-    var games: List<Game>
-) : RecyclerView.Adapter<GameAdapter.GameViewHolder>() {
+class GameAdapter (var games: ArrayList<Game>) : RecyclerView.Adapter<GameAdapter.GameViewHolder>()
+{
 
     inner class GameViewHolder(val binding: ItemGameBinding) : RecyclerView.ViewHolder(binding.root)
-
     // if the user scrolled little bit and another item was recycled and new item needs to be viewed
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
         // it will crash if we wouldn't have set last parameter as false in rv
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = ItemGameBinding.inflate(layoutInflater, parent, false)
+
         return GameViewHolder(binding)
 
     }
-
     // sets the text and other viewed stuff initially
     override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
         holder.binding.apply {
+
             tvGame.text = games[position].title
             tvScore.text = games[position].score
             tvType.text = games[position].type
-            Glide.with(this.ivImage).load(games.get(position).imageURL).into(ivImage)
-
+            Glide.with(this.ivImage).load(games[position].imageURL).into(ivImage)
         }
-        holder.itemView.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                val activity = v!!.context as AppCompatActivity
-                val gameDetails=GameDetails()
-                activity.supportFragmentManager.beginTransaction().apply {
-                    replace(R.id.main,gameDetails).addToBackStack(null).commit()
-                }
-            }
-        })
-
 
     }
-
     // how many items we have currently in recyclerview,
     override fun getItemCount(): Int {
         return games.size
     }
+
+    fun filterList(filterlist: ArrayList<Game>) {
+        // below line is to add our filtered
+        // list in our course array list.
+        games = filterlist
+    }
+
 }
