64ec9bdbc66f6e3b8439d86d3e9b870eedd2847e
Emir <emiryegnidemir@hotmail.com>
2022-12-13 02:15:15 +0300

    add UI improvements and search functionality

--- app/src/main/java/com/example/projectx/GameAdapter.kt
+++ app/src/main/java/com/example/projectx/GameAdapter.kt
@@ -1,13 +1,8 @@
 package com.example.projectx
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.OnClickListener
 import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.projectx.databinding.ItemGameBinding
