e39c8cc302947bab69f959ec3fa36b7732fb381d
oguzgokcen <oguz-2002@windowslive.com>
2022-12-22 19:16:03 +0300

    favori ekleme işlemini gerçekleştirme

--- app/src/main/java/com/example/projectx/FavoritesFragment.kt
+++ app/src/main/java/com/example/projectx/FavoritesFragment.kt
@@ -1,43 +1,51 @@
 package com.example.projectx
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import java.util.ArrayList
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
 
 
-class FavoritesFragment : Fragment(),onClickListener {
+class FavoritesFragment : Fragment(), onClickListener {
     lateinit var rvFav : RecyclerView
     lateinit var gameAdapter: GameAdapter
     lateinit var favoritedList: ArrayList<Game>
+    lateinit var tempfavoritedList: ArrayList<Game>
     lateinit var favouritesText: TextView
-    
+    lateinit var sp: SharedPreferences
+    lateinit var spct:SharedPreferences
     @SuppressLint("SetTextI18n")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_favorites, container , false)
         rvFav = view.findViewById(R.id.rvFav)
-        favoritedList = ArrayList()
         favouritesText = view.findViewById(R.id.favouritesText)
-        //addGamestoList()
-        /*val bundle = arguments
-        val name = bundle!!.getString("name")
-        val imageUrl = bundle.getString("imageUrl")
-        val gameDesc = bundle.getString("gameDesc")
-        val redditLink =bundle.getString("redditLink")
-        val webLink = bundle.getString("webLink")
-        val score =bundle.getString("score")
-        val type = bundle.getString("type")
-        favoritedList.add(Game(name!!,imageUrl!!,gameDesc!!,redditLink!!,webLink!!,score!!,type!!))
+        sp = this.requireActivity().getSharedPreferences("sharedpref", Context.MODE_PRIVATE)
+        spct = this.requireActivity().getSharedPreferences("sharedprefct", Context.MODE_PRIVATE)
+        val ct =spct.getInt("ct",0)
+        favoritedList = ArrayList()
+        if (ct>0) { // fav listesine eklenecek varsa
+            val gson = Gson()
+            val json = sp.getString("favGames", null)
+            val type: Type = object : TypeToken<ArrayList<Game>>() {}.type
+            tempfavoritedList = ArrayList()
+            tempfavoritedList = gson.fromJson(json,type)
+            for (games in tempfavoritedList){
+                favoritedList.add(games) // ekleme
+            }
+        }
 
-         */
         favouritesText.text = "Favorites(${favoritedList.count()})"
         rvFav.setHasFixedSize(true)
         rvFav.layoutManager = LinearLayoutManager(view.context)
@@ -45,34 +53,16 @@ class FavoritesFragment : Fragment(),onClickListener {
         rvFav.adapter = gameAdapter
         return view
     }
-    /*fun addGamestoList(){
-        favoritedList = ArrayList()
-        favoritedList.add(Game("Grand Theft Auto V",
-            "https://s3-alpha-sig.figma.com/img/23a0/2645/15fb5b947775dad15186f70457f39007?Expires=1671408000&Signature=IRXqA7tsEhkNwYg1-bvMD1yHeSfW2s6jRZY~8GQGOl6DXUN5yrNMXf~GlzzK~4AoGAQHWrnc4fvDPJVNw0oEkRGEx~heBXtM7LQmDmfPP1ztOTuDWY4huLgwGdae4FUf5kWYLvgAGGR52-B6KmxobmTkI~sW2~5bbl16asZdjXTxlu71WwhYDHDj7PdSLHbgU-6CMFzOzM0qwCjlAGIT9UZIhT9phl6-UISET3TCYznN8cAOX2npWe0jWeVuRNzWoG003eOVsZdlL8kcgQOe8wAOTUY~NFFtXG55yzUyujVSCtCM-FbFRd5lPoMFDUVr6rfo1isO5D97M11-prklSg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
-            "Los Santos: a sprawling sun-soaked metropolis full of self-help gurus," +
-                    " starlets and fading celebrities, once the envy of the Western world, " +
-                    "now struggling to stay alive in a time of economic uncertainty and cheap reality TV. " +
-                    "Amidst the turmoil, three very unique criminals plot their own chances of survival and success: " +
-                    "Franklin, a street hustler looking for tangible opportunities and serious money; " +
-                    "Michael, a professional ex-con whose retirement is less rosy than he figured it would be; and Trevor, " +
-                    "a violent dude driven by the opportunity for a cheap high and his next big score. With options at a premium, " +
-                    "the crew risks it all in a myriad of daring and dangerous heists that could set them up for life.",
-            "https://www.reddit.com/submit?url=https%3A%2F%2Fwww.metacritic.com%2Fgame%2Fpc%2Fgrand-theft-auto-v%3Fftag%3Dredsoshares&title=Grand%20Theft%20Auto%20V%20for%20PC%20Reviews",
-            "https://www.rockstargames.com/gta-v",
-            "96",
-            "Action, shooter"))
-        favoritedList.add( Game("Portal 2",
-            "https://s3-alpha-sig.figma.com/img/179b/f4d6/5e080b5bd8adfa7aeabe119671e8542b?Expires=1672012800&Signature=ANFJebCTPG6GkU3nHVg~UWu-0ONsGRGaqgIskwWQSO6Op2yGmNayvdP4eisFj1mg2UqGlDdbK8WUYhcWWNcSUN-hQbiDtUXR~Qs0zLcU3OdD06rikpGJ0OWoG~n3K58m1NB~asyJEGFzg~WRojZxpJKiSW~VobCwWkaZz6hB7XFvn-aIxOUQk2ekNo71pLqfi6K3X1Qrqnf6lLAtZupT8V4fKLDkb-ygHGy0JsYBPQBDsTg6laugfhFeAXxCs7V69PZq~Jc-7BnmTHr1q3C2HEK7VMlfhsdY17q~~sttsrnvAuD4e9uEusAWODdk~gFhaBprytOwRaFAujpBu3yrEA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
-            "Valve is working on a full-length sequel to its award-winning, mind-bending puzzle action game for the Xbox 360 and PC.",
-            "https://www.reddit.com/submit?url=https%3A%2F%2Fwww.metacritic.com%2Fgame%2Fpc%2Fportal-2%3Fftag%3Dredsoshares&title=Portal%202%20Details%20and%20Credits%20for%20PC",
-            "https://www.thinkwithportals.com/about.php",
-            "95",
-            "Action, puzzle"))
-    }*/
-
     override fun onGameClickListener(position: Int,v: View?) { // TAMAMLANMADI
         val actv = v?.context as AppCompatActivity
         val intent = Intent(actv,DetailActivity::class.java)
+        intent.putExtra("name",gameAdapter.games[position].title)
+        intent.putExtra("imageUrl",gameAdapter.games[position].imageURL)
+        intent.putExtra("gameDesc",gameAdapter.games[position].game_desc)
+        intent.putExtra("redditLink",gameAdapter.games[position].reddit_link)
+        intent.putExtra("webLink",gameAdapter.games[position].web_link)
+        intent.putExtra("score",gameAdapter.games[position].score)
+        intent.putExtra("type",gameAdapter.games[position].type)
         startActivity(intent)
     }
 }
--- app/src/main/java/com/example/projectx/GameFragment.kt
+++ app/src/main/java/com/example/projectx/GameFragment.kt
@@ -1,6 +1,8 @@
 package com.example.projectx
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
